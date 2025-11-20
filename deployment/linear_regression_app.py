85b4e89f7ed5c9d77a6a53515b802956747f37f5
Mukund Garg <gargmukund774@gmail.com>
2025-11-21 00:10:43 +0530

    Added Streamlit linear regression app

--- deployment/linear_regression_app.py
+++ deployment/linear_regression_app.py
@@ -1,12 +1,25 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-df = pd.read_csv("data/canada.csv")
+
+df = pd.read_csv("../data/canada.csv")
+st.write(df.head())  # Streamlit-friendly print
+
 plt.ylabel("per capita income")
 plt.scatter(df["year"], df["per capita income (US$)"], color="red", marker='+')
 
-reg=linear_model.LinearRegression()
+reg = linear_model.LinearRegression()
 p = reg.fit(df[["year"]], df["per capita income (US$)"])
 plt.plot(df["year"], reg.predict(df[["year"]]), color='black')
-plt.show()
+
+# Show plot in Streamlit
+st.pyplot(plt)
+
+# Prediction UI
+year = st.number_input("Enter year:", min_value=1900, max_value=2100, value=2025)
+
+if st.button("Predict"):
+    prediction = reg.predict([[year]])
+    st.success(f"Predicted Per Capita Income for {year}: **${prediction[0]:.2f}**")

9a10d8fb0d4247efbafb9ef1036a3d6fb5c4eb0a
Mukund Garg <gargmukund774@gmail.com>
2025-11-20 23:36:33 +0530

    Added project structure + Streamlit deployment setup for Linear Regression

---
+++ deployment_linear_regression/app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+df = pd.read_csv("/Users/mukundgarg/Desktop/Machine learning/data/canada.csv")
+print(df)
+plt.xlabel("year")
+plt.ylabel("per capita income")
+plt.scatter(df["year"], df["per capita income (US$)"], color="red", marker='+')
+
+reg=linear_model.LinearRegression()
+p = reg.fit(df[["year"]], df["per capita income (US$)"])
+plt.plot(df["year"], reg.predict(df[["year"]]), color='black')
+plt.show()
